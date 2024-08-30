56b7f229c521672c1aa7a36c5f116d573b86d813
bhatta09 <bsandesh345@gmail.com>
2024-08-30 22:40:12 +0545

    login & signup design complete

--- src/components/AuthBg.jsx
+++ src/components/AuthBg.jsx
@@ -1,11 +1,12 @@
-
+import img from "../assets/room.png"
 const AuthBg = () => {
   return (
     <div>
-      <h1 className="font-bold text-3xl font-serif pb-10 pt-11">Welcome to <span className="text-yellow-400">Room <br /> Rental</span> System.</h1>
-      <p>Rent smarter with tailored spaces that suit your lifestyle.
+      <h1 className="font-bold text-4xl font-serif pb-10 pt-11 pl-10">Welcome to <span className="text-yellow-400">Room <br /> Rental</span> System.</h1>
+      <p className="pl-10">Rent smarter with tailored spaces that suit your lifestyle.
 Live better in a home that truly feels like yours.
 </p>
+<img src={img} alt="" className="mt-16"/>
     </div>
   )
 }

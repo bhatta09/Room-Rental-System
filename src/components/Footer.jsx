3142d352c0c6133384e869cbb3c0e1562240728a
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-09-08 22:25:03 +0545

    newsletterAdded

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,7 +41,25 @@ const footerData = [
 const Footer = () => {
   return (
     <div className="mt-4  bg-[#f5f5fe]">
-      <div className="flex flex-row  px-32 mt-4 mb-10 gap-48 ">
+      <div className="flex flex-row gap-56 bg-orange-400 py-12 px-32 ">
+        <div>
+          <h3 className="text-2xl  font-semibold">
+            Subscribe to our newsletter and we’ll keep you up to date on <br />{" "}
+            our products and services
+          </h3>
+        </div>
+        <div className=" bg-white h-12 rounded-full">
+          <input
+            type="text"
+            placeholder="Enter Your Email Address"
+            className="rounded-l-full w-80 placeholder:font-bold px-4   "
+          />
+          <button className="bg-yellow-400  h-12 w-12 text-black font-bold text-3xl rounded-full hover:bg-yellow-500 ">
+            →
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row  px-32 mt-4 pt-4 mb-10 gap-48 ">
         <div className="flex flex-row gap-32">
           <div className="flex flex-col gap-5 text-[12px]">
             <h3 className=" font-bold text-xl text-yellow-400">

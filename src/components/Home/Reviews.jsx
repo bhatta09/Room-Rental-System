23722205c9d91cfdc4b4d017ed19c807c8877ad7
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-09-14 19:50:08 +0545

    whyChooseUs&reviewUIupdated

--- src/components/Home/Reviews.jsx
+++ src/components/Home/Reviews.jsx
@@ -1,18 +1,29 @@
 const Reviews = (props) => {
-    return (
-      <div className='w-full md:w-1/3 bg-white border-2 border-lightText md:border-none p-5 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
-          <div>
-              <p className='text-lightText'>The room rental system offers a streamlined and
-                 intuitive experience, making it easy for users to browse, compare, and book rooms effortlessly.
-                 With clear room details and a responsive design, it provides a hassle-free solution for finding accommodations quickly.
-                   </p>
-          </div>
-          <div className='flex flex-row justify-center items-center mt-4 gap-4'>
-              <img className='rounded-full w-1/4' src={props.img} alt="img" />
-              <h3 className='font-semibold'>{props.name}</h3>
-          </div>
+  const renderStars = (props) => {
+    return "⭐".repeat(props); // Repeat the star emoji based on the stars prop
+  };
+  return (
+    <div className="flex flex-col  p-6 bg-white rounded-lg shadow-lg ">
+      <div className="flex items-center space-x-2">
+        {/* Star ratings */}
+        <span>{renderStars(props.star)}</span>
       </div>
-    )
-  }
-  
-  export default Reviews;
+      <p className="mt-4 text-sm font-normal italic text-justify text-gray-500 w-80 h-36">
+        {props.desc}
+      </p>
+      <div className="flex items-center mt-4 space-x-2 border-t-2">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1687989650423-49cf039fe80a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="User profile"
+          className="w-10 h-10 rounded-full"
+        />
+        <div>
+          <h4 className="font-semibold">{props.name}</h4>
+          <p className="text-sm text-gray-500">{props.work}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,9 +19,11 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { setToken } from "../redux/auth/authSlice";
-
+import VerifiedOutlinedIcon from "@mui/icons-material/VerifiedOutlined";
+import WhereToVoteOutlinedIcon from "@mui/icons-material/WhereToVoteOutlined";
 import NorthIcon from "@mui/icons-material/North";
-
+import PriceCheckOutlinedIcon from "@mui/icons-material/PriceCheckOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 const NextArrow = ({ onClick }) => (
   <div
     className="absolute right-[-10px] top-1/2 transform -translate-y-1/2 cursor-pointer"
@@ -219,40 +221,75 @@ const Home = () => {
         <Agent />
       </section>
 
-      <section>
-        <h1 className="text-4xl font-semibold text-center pt-24 pb-10">
-          Customor's Review
+      <section className="">
+        <h1 className="text-lg font-semibold text-center text-gray-500 pt-10 ">
+          3,012 people have said how good NayaAawas
         </h1>
-        <div className="flex flex-col md:flex-row gap-4 mt-5 mx-3">
-          <Reviews img={img} name="Pritam" />
-          <Reviews img={img} name="Swornim" />
-          <Reviews img={img} name="Bijaya" />
+
+        <h1 className="text-4xl font-semibold text-center pb-10">
+          Our happy clients say about us
+        </h1>
+        <div className="relative flex flex-col md:flex-row gap-8 mt-5 mx-3 px-12 lg:px-32 p-8 rounded-lg justify-center">
+          <div className="absolute inset-0 bg-gradient-to-r from-white via-yellow-500 to-white blur-lg rounded-lg"></div>
+          <div className="relative z-10 flex flex-wrap md:flex-row gap-8 justify-center">
+            {/* Testimonial 1 */}
+            <Reviews
+              name="Swornim Shrestha"
+              work="React Developer"
+              star="4"
+              desc="The room rental system offers a streamlined and intuitive experience,
+        making it easy for users to browse, compare, and book rooms
+        effortlessly. With clear room details and a responsive design"
+            />
+            <Reviews
+              name="Sandesh Shrestha"
+              work="React Developer"
+              star="3"
+              desc="The room rental system offers a streamlined and intuitive experience,
+        making it easy for users to browse, compare, and book rooms
+        effortlessly. With clear room details and a responsive design, it
+        provides a hassle-free solution for finding accommodations quickly."
+            />
+            <Reviews
+              name="Pritam Shrestha"
+              work="Java Developer"
+              star="5"
+              desc="The best room renting website i have ever discover, it
+        provides a hassle-free solution for finding accommodations quickly."
+            />
+          </div>
         </div>
       </section>
 
-      <section className="p-8">
-        <h2 className="text-3xl font-semibold text-center mb-8">
+      <section className="my-10">
+        <h2 className="text-3xl  font-semibold  mb-8  text-center ml-[-40px] ">
           Why Choose Us?
         </h2>
-        <div className="flex justify-around">
-          <div className="text-center">
-            <div className="text-5xl mb-4">🌟</div>
+        <div className="flex  justify-between  mx-12 lg:mx-32">
+          <div className="flex flex-col items-center">
+            <div className="text-5xl mb-4 border-[4px] border-yellow-400 rounded-full w-20 h-20 flex items-center justify-center ">
+              <WhereToVoteOutlinedIcon sx={{ fontSize: 50, color: "black" }} />
+            </div>
             <h3 className="text-xl font-semibold">Best Locations</h3>
-            <p className="text-gray-600">
+            <p className="text-gray-600 hidden lg:block">
               All our rooms are in prime locations.
             </p>
           </div>
-          <div className="text-center">
-            <div className="text-5xl mb-4">💰</div>
+          <div className="flex flex-col items-center">
+            <div className="text-5xl mb-4 border-[4px] border-yellow-400 rounded-full w-20 h-20 flex items-center justify-center ">
+              <ShoppingBagOutlinedIcon sx={{ fontSize: 50, color: "black" }} />
+            </div>
             <h3 className="text-xl font-semibold">Affordable Prices</h3>
-            <p className="text-gray-600">
+            <p className="text-gray-600 hidden lg:block">
               We offer competitive prices for all room types.
             </p>
           </div>
-          <div className="text-center">
-            <div className="text-5xl mb-4">👍</div>
+          <div className="flex flex-col items-center">
+            <div className="text-5xl mb-4 border-[4px] border-yellow-400 rounded-full w-20 h-20 flex items-center justify-center ">
+              <VerifiedOutlinedIcon sx={{ fontSize: 50, color: "black" }} />
+            </div>
             <h3 className="text-xl font-semibold">Trusted Hosts</h3>
-            <p className="text-gray-600">
+            <p className="text-gray-600 hidden lg:block">
               Our hosts are verified for safety and quality.
             </p>
           </div>

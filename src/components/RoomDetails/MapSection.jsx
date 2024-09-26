44d614ffddfe838873eb4ae8b1cf57499fec36e3
bhatta09 <bsandesh345@gmail.com>
2024-09-26 19:21:53 +0545

    room detail page update

--- src/components/RoomDetails/MapSection.jsx
+++ src/components/RoomDetails/MapSection.jsx
@@ -1,15 +1,16 @@
 const MapSection = () => {
-    return (
-      <div className="mt-4">
-        <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14135.623272447838!2d85.34808395528654!3d27.65838546936721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb10aa668e649f%3A0x302b443de16adbd5!2sTikathali%2C%20Mahalaxmi!5e0!3m2!1sen!2snp!4v1725275890620!5m2!1sen!2snp" 
-          width="100%" 
-          height="200" 
-          allowFullScreen="" 
-          loading="lazy" 
-          className="border rounded-lg"
-        ></iframe>
-      </div>
-    );
-  };
-export default MapSection;  
+  return (
+    <div className="mt-4">
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14135.623272447838!2d85.34808395528654!3d27.65838546936721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb10aa668e649f%3A0x302b443de16adbd5!2sTikathali%2C%20Mahalaxmi!5e0!3m2!1sen!2snp!4v1725275890620!5m2!1sen!2snp" 
+        width="100%" 
+        height="200" 
+        allowFullScreen="" 
+        loading="lazy" 
+        className="border rounded-lg"
+      ></iframe>
+    </div>
+  );
+};
+
+export default MapSection;
--- src/components/RoomDetails/SellerProfile.jsx
+++ src/components/RoomDetails/SellerProfile.jsx
@@ -1,19 +1,19 @@
-
 const SellerProfile = () => {
-    return (
-      <div className="border p-4 rounded-lg">
-        <div className="flex items-center mb-4">
-          <img src="" alt="Profile" className="rounded-full mr-4" />
-          <div>
-            <h4 className="text-lg font-semibold">Swornim Shrestha</h4>
-            <p className="text-gray-600">9869348462</p>
-            <p className="text-gray-600">swornim@academiacollege.edu.np</p>
-          </div>
+  return (
+    <div className="border p-4 rounded-lg">
+      <div className="flex items-center mb-4">
+        <img src="" alt="Profile" className="rounded-full mr-4" />
+        <div>
+          <h4 className="text-lg font-semibold">Swornim Shrestha</h4>
+          <p className="text-gray-600">9869348462</p>
+          <p className="text-gray-600">swornim@academiacollege.edu.np</p>
         </div>
-        <button className="bg-yellow-400 text-white px-4 py-2 rounded-md w-full">
-          View Profile
-        </button>
       </div>
-    );
-  };
-export default SellerProfile  ;
+      <button className="bg-yellow-400 text-white px-4 py-2 rounded-md w-full">
+        View Profile
+      </button>
+    </div>
+  );
+};
+
+export default SellerProfile;

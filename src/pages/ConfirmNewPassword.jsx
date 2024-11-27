8b185ce5ed97aba16d3a09f1bb44b88d596f9d1d
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-11-28 00:02:43 +0545

    add a feature to reset password

--- src/pages/ConfirmNewPassword.jsx
+++ src/pages/ConfirmNewPassword.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthBg from "../components/Auth/AuthBg";
 import { TextField } from "@mui/material";
+import axios from "axios";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const ConfirmNewPassword = () => {
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `/api/v1/auth/update/password?token=${token}`,
+        {
+          newPassword,
+          confirmPassword,
+        }
+      );
+      toast.success("Reset Successfully");
+      setConfirmPassword("");
+      setNewPassword("");
+      setFormErrors("");
+      navigate("/");
+    } catch (error) {
+      if (error.response && error.response.data) {
+        const errors = error.response.data;
+        setFormErrors(errors);
+      } else {
+        setFormErrors({
+          general: "An unexpected error occurred",
+        });
+      }
+    }
+  };
   return (
     <div className="flex mx-auto justify-center flex-wrap mb-24">
       <div className="mt-5">
@@ -20,13 +55,30 @@ const ConfirmNewPassword = () => {
         <div>
           <div className="mb-3 font-semibold flex flex-col gap-2">
             <label className="text-xs font-bold">New Password *</label>
-            <TextField type="email" placeholder="Enter Password" />
+            <TextField
+              type="email"
+              placeholder="Enter Password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              error={!!formErrors.errorMsg || formErrors.newPassword}
+              helperText={formErrors.errorMsg || formErrors.newPassword}
+            />
           </div>
           <div className="mb-3 font-semibold flex flex-col gap-2">
             <label className="text-xs font-bold">Confirm Password*</label>
-            <TextField type="email" placeholder=" Confirm Password" />
+            <TextField
+              type="email"
+              placeholder=" Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              error={!!formErrors.errorMsg}
+              helperText={formErrors.errorMsg}
+            />
           </div>
-          <button className="w-full font-semibold bg-[#ffb200] rounded-lg h-10 text-base font-sm text-white">
+          <button
+            onClick={handleSubmit}
+            className="w-full font-semibold bg-[#ffb200] rounded-lg h-10 text-base font-sm text-white"
+          >
             Reset Password
           </button>
         </div>
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,41 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthBg from "../components/Auth/AuthBg";
 import { TextField } from "@mui/material";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import ConfirmEmailBtn from "./ConfirmEmailtBtn";
 
 const ResetPassword = () => {
-  return (
-    <div className="flex mx-auto justify-center flex-wrap mb-24">
-      <div className="mt-5">
-        <AuthBg />
-      </div>
-      <div className="max-w-lg w-full  p-5 mt-10 md:ml-10 bg-[#fafbfb] bg-opacity-80 shadow-xl rounded-lg mb-4">
-        <h2 className="text-2xl font-normal mb-6 text-gray-800 text-center uppercase">
-          Reset Password
-        </h2>
-        <div>
-          <div className="mb-3 font-semibold flex flex-col gap-2">
-            <label className="text-xs font-bold">Email Address*</label>
-            <TextField type="email" placeholder="Enter your email address" />
-          </div>
+  const [email, setEmail] = useState("");
+  const [openDialog, setopenDialog] = useState(false);
+  const [formErrors, setFormErrors] = useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `/api/v1/auth/reset/password?email=${email}`
+      );
 
-          <button className="w-full font-semibold bg-[#ffb200] rounded h-10 text-base font-sm text-white">
-            Send Link
-          </button>
-          <div className="flex justify-center gap-2 text-sm mt-5">
-            <span className="text-gray-500 font-semibold">
-              We’ll send you a link to your email to reset your password{" "}
-              <Link
-                to="/confirm-new-password"
-                className="text-yellow-400 font-semibold "
-              >
-                Test
-              </Link>
-            </span>
+      setopenDialog(true);
+    } catch (error) {
+      if (error.response && error.response.data) {
+        const errors = error.response.data;
+        setFormErrors(errors);
+      } else {
+        setFormErrors({
+          general: "An unexpected error occurred",
+        });
+      }
+    }
+  };
+  // const handleLogout = () => {
+  //   setopenDialog(true);
+  // };
+  return (
+    <>
+      <div className="flex mx-auto justify-center flex-wrap mb-24">
+        <div className="mt-5">
+          <AuthBg />
+        </div>
+        <div className="max-w-lg w-full  p-5 mt-10 md:ml-10 bg-[#fafbfb] bg-opacity-80 shadow-xl rounded-lg mb-4">
+          <h2 className="text-2xl font-normal mb-6 text-gray-800 text-center uppercase">
+            Reset Password
+          </h2>
+          <div>
+            <div className="mb-3 font-semibold flex flex-col gap-2">
+              <label className="text-xs font-bold">Email Address*</label>
+              <TextField
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Enter your email address"
+              />
+            </div>
+
+            <button
+              onClick={handleSubmit}
+              className="w-full font-semibold bg-[#ffb200] rounded h-10 text-base font-sm text-white"
+            >
+              Send Link
+            </button>
+            <div className="flex justify-center gap-2 text-sm mt-5">
+              <span className="text-gray-500 font-semibold">
+                We’ll send you a link to your email to reset your password{" "}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <ConfirmEmailBtn openDialog={openDialog} setopenDialog={setopenDialog} />
+    </>
   );
 };
 
